f7ee5211daca67b4e150e4f9965d51f3c12e5bb1
vtb4036303 <ushotin@dev.vtb.ru>
2024-05-03 09:29:09 +0400

    improved task 1 top products

--- java-interview/ru/shotin/tasks/Task1OrdersTopProducts.java
+++ java-interview/ru/shotin/tasks/Task1OrdersTopProducts.java
@@ -7,35 +7,46 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static ru.shotin.tasks.Task1OrdersTopProducts.ProductNames.*;
+
 public class Task1OrdersTopProducts {
 
     public static void printTop2ProductsOfMonth(List<CommerceOrder> commerceOrders, Month month) {
         System.out.println("=== Top 2 Product of " + month + " ===");
         // TODO: print ${productName}=${sum(count)}
-        System.out.println(commerceOrders);
     }
 
     public static void main(String[] args) {
         List<CommerceOrder> commerceOrders = new ArrayList<>();
-        commerceOrders.add(new CommerceOrder(LocalDate.parse("2024-01-09"), Arrays.asList(
-                new CommerceItem("pears", 2), new CommerceItem("apples", 3)))
-        );
         commerceOrders.add(new CommerceOrder(LocalDate.parse("2024-03-12"), Arrays.asList(
-                new CommerceItem("apples", 3), new CommerceItem("babanas", 1)))
+                new CommerceItem(apples, 3), new CommerceItem(bananas, 1)))
+        );
+        commerceOrders.add(new CommerceOrder(LocalDate.parse("2024-02-21"), Arrays.asList(
+                new CommerceItem(pears, 3), new CommerceItem(apples, 1), new CommerceItem(bananas, 1)))
+        );
+        commerceOrders.add(new CommerceOrder(LocalDate.parse("2024-01-09"), Arrays.asList(
+                new CommerceItem(pears, 2), new CommerceItem(apples, 3)))
         );
         commerceOrders.add(new CommerceOrder(LocalDate.parse("2024-03-14"), Arrays.asList(
-                new CommerceItem("babanas", 1)))
+                new CommerceItem(bananas, 1), new CommerceItem(pears, 2), new CommerceItem(apples, 3)))
         );
         commerceOrders.add(new CommerceOrder(LocalDate.parse("2024-02-16"), Arrays.asList(
-                new CommerceItem("pears", 2), new CommerceItem("babanas", 5)))
+                new CommerceItem(pears, 2), new CommerceItem(bananas, 5), new CommerceItem(apples, 1)))
         );
         commerceOrders.add(new CommerceOrder(LocalDate.parse("2024-03-09"), Arrays.asList(
-                new CommerceItem("apples", 1)))
+                new CommerceItem(apples, 1)))
         );
         commerceOrders.add(new CommerceOrder(LocalDate.parse("2024-03-15"), Arrays.asList(
-                new CommerceItem("pears", 3), new CommerceItem("babanas", 7)))
+                new CommerceItem(pears, 5), new CommerceItem(bananas, 3)))
         );
+        System.out.println(commerceOrders);
+        printTop2ProductsOfMonth(commerceOrders, Month.JANUARY);
+        printTop2ProductsOfMonth(commerceOrders, Month.FEBRUARY);
         printTop2ProductsOfMonth(commerceOrders, Month.MARCH);
+    }
+
+    interface ProductNames {
+        String apples = "apples", bananas = "bananas", pears = "pears";
     }
 
     static class CommerceItem {
